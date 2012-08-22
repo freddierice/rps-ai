6884520b03ac5dc39edddd94f8aefd89554d907e
frice2014 <freddie.rice@gmail.com>
2012-08-21 23:33:03 -0500

    basic structure

--- rps.py
+++ rps.py
@@ -1,10 +1,27 @@
+#RockSolid
 #created by Freddie Rice
 
-#if this is the first round
+#------------------ Imports ------------------#
+import random
+
+#---------------- Check First ----------------#
 if input == '' :
-  output = random.choice( 'R', 'S', 'P' )
+  output = random.choice( ['R', 'S', 'P'] )
+  p_list = [ input[:] ]
+  c_list = [ output[:] ]
   rounds_played = 1
-  player_list = [ input[:] ]
-  comp_list = [ output[:] ]
-else
+elif rounds_played <= 5 :
+  output = random.choice( ['R', 'S', 'P'] )
+  p_list = p_list + [ input[:] ]
+  c_list = c_list + [ output[:] ]
   rounds_played += 1
+else
+  #setup the round
+  rounds_played += 1
+  #print rounds_played
+  p_list = p_list + [ input[:] ]
+  
+  
+  
+  output = random.choice( ['R', 'S', 'P'] )
+  c_list = c_list + [ output[:] ]
